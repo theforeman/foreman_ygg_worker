d0c28f54abacd087f8f7241fa0a5fce8ce080dd4
Ondřej Ezr <oezr@redhat.com>
2021-04-22 18:16:43 +0200

    Add publish script

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # Foreman worker for yggdrasil
 
-This is a worker service for yggdrasil that acts as pull client for Foreman.
+This is a POC worker service for yggdrasil that can act as pull client for Foreman.
+
+This is not meant for production use, only as a simple example how to hook into the ygg.
+
+## Hacking
+
+Client is in `foreman_worker`, that includes `yggdrassil_pb.rb` and `yggdrasil_services_pb.rb` to load the gRPC definitions.
+
+For the server side we have the simle `publish_notify.rb` to play around with the server/client MQTT around and get the idea what is going on under the hood.
+IRL it would be hooked into the smart_proxy, that will be notifying about the real jobs.
 
 
-## Developement
+### Preparing gRPC
+
+This is needed only if the ygg protocol will ever change.
 
 Refreshing message and services definitions from proto:
 
--- publish_notify.rb
+++ publish_notify.rb
@@ -1,19 +1,28 @@
-BROKER = 'localhost'
+#!/usr/bin/env ruby
+
+require 'mqtt'
+require 'date'
+require 'securerandom'
+require 'json'
+
+BROKER = 'centos8-katello-4-0.oezr-fedora.example.com'
 BROKER_PORT = 1883
 
 CLIENT_UUID = '6deda2c6-244a-482c-84ea-9f940e88432c'
 
 message = {
-  type: 'work',
-  client_uuid: CLIENT_UUID,
+  type: 'data',
+  message_id: SecureRandom.uuid,
+  # client_uuid doesn't seemt to be used
+  # client_uuid: CLIENT_UUID,
   version: 1,
-  sent: Time.now.iso8601,
-  payload: {
-    handler: 'foreman',
-  }
+  sent: DateTime.now.iso8601,
+  directive: 'foreman',
+  content: 'https://raw.githubusercontent.com/ezr-ondrej/foreman_ygg_worker/main/work',
 }
 
+topic = "yggdrasil/#{CLIENT_UUID}/data/in"
 
 MQTT::Client.connect(BROKER, BROKER_PORT) do |c|
-  c.publish("per-host/#{@host}", JSON.dump(message), false, 1)
+  c.publish(topic, JSON.dump(message), false, 1)
 end
