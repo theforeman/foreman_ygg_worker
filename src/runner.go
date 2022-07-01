0b7bba07bd6e198eebe6459c0beacccfe91ed126
Adam Ruzicka <a.ruzicka@outlook.com>
2022-07-01 15:59:34 +0200

    Refactor pre-start failure error handling
    Before this change, Smart Proxy (and therefore Foreman) would never know
    if the client failed before it actually kicked off the job.

--- src/runner.go
+++ src/runner.go
@@ -15,6 +15,8 @@ import (
 	pb "github.com/redhatinsights/yggdrasil/protocol"
 )
 
+const preStartErrorExitCode = 254
+
 func dispatch(ctx context.Context, d *pb.Data, s *jobStorage) {
 	event, prs := d.GetMetadata()["event"]
 	if !prs {
@@ -43,26 +45,35 @@ func startScript(ctx context.Context, d *pb.Data, s *jobStorage) {
 	script := string(d.GetContent())
 	log.Tracef("running script : %#v", script)
 
+	updates := make(chan V1Update)
+
+	oa := NewUpdateAggregator(d.GetMetadata()["return_url"], d.GetMessageId())
+	go oa.Aggregate(updates, &YggdrasilGrpc{})
+
 	scriptfile, err := ioutil.TempFile("/tmp", "ygg_rex")
 	if err != nil {
-		log.Errorf("failed to create script tmp file: %v", err)
+		reportStartError(fmt.Sprintf("failed to create script tmp file: %v", err), updates)
+		return
 	}
 	defer os.Remove(scriptfile.Name())
 
 	n2, err := scriptfile.Write(d.GetContent())
 	if err != nil {
-		log.Errorf("failed to write script to tmp file: %v", err)
+		reportStartError(fmt.Sprintf("failed to write script to tmp file: %v", err), updates)
+		return
 	}
 	log.Debugf("script of %d bytes written in : %#v", n2, scriptfile.Name())
 
 	err = scriptfile.Close()
 	if err != nil {
-		log.Fatal(err)
+		reportStartError(fmt.Sprintf("%v", err), updates)
+		return
 	}
 
 	err = os.Chmod(scriptfile.Name(), 0700)
 	if err != nil {
-		log.Fatal(err)
+		reportStartError(fmt.Sprintf("%v", err), updates)
+		return
 	}
 
 	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("cd ~; export HOME=\"$PWD\"; exec %v", scriptfile.Name()))
@@ -70,20 +81,21 @@ func startScript(ctx context.Context, d *pb.Data, s *jobStorage) {
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		log.Errorf("cannot connect to stdout: %v", err)
+		reportStartError(fmt.Sprintf("cannot connect to stdout: %v", err), updates)
 		return
 	}
 
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
-		log.Errorf("cannot connect to stderr: %v", err)
+		reportStartError(fmt.Sprintf("cannot connect to stderr: %v", err), updates)
 		return
 	}
 
 	if err := cmd.Start(); err != nil {
-		log.Errorf("cannot run script: %v", err)
+		reportStartError(fmt.Sprintf("cannot run script: %v", err), updates)
 		return
 	}
+
 	log.Infof("started script process: %v", cmd.Process.Pid)
 	if jobUUIDP {
 		s.Set(jobUUID, cmd.Process.Pid)
@@ -92,11 +104,6 @@ func startScript(ctx context.Context, d *pb.Data, s *jobStorage) {
 
 	var wg sync.WaitGroup
 	wg.Add(2)
-
-	updates := make(chan V1Update)
-
-	oa := NewUpdateAggregator(d.GetMetadata()["return_url"], d.GetMessageId())
-	go oa.Aggregate(updates, &YggdrasilGrpc{})
 
 	go func() { outputCollector("stdout", stdout, updates); wg.Done() }()
 	go func() { outputCollector("stderr", stderr, updates); wg.Done() }()
@@ -109,13 +116,21 @@ func startScript(ctx context.Context, d *pb.Data, s *jobStorage) {
 				updates <- NewExitUpdate(status.ExitStatus())
 			}
 		} else {
-			log.Errorf("script run failed: %v", err)
+			reportStartError(fmt.Sprintf("script run failed: %v", err), updates)
+			return
 		}
 	} else {
 		updates <- NewExitUpdate(0)
 	}
 	close(updates)
 	log.Infof("Finished job %v", jobUUID)
+}
+
+func reportStartError(message string, updates chan<- V1Update) {
+	log.Error(message)
+	updates <- NewOutputUpdate("stderr", message)
+	updates <- NewExitUpdate(preStartErrorExitCode)
+	close(updates)
 }
 
 func outputCollector(stdtype string, pipe io.ReadCloser, outputs chan<- V1Update) {
